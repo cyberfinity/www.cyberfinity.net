f65ad57396741072bfc6cf252ed08121825a1e85
James Nash <cirrus@twiddles.com>
2019-01-12 17:40:31 +0000

    Added baseline visualisation to vertical rythmn preview.

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: 'stylelint-config-sass-guidelines',
   rules: {
     'declaration-no-important': true,
+    'function-parentheses-space-inside': 'never-single-line',
   },
 };
